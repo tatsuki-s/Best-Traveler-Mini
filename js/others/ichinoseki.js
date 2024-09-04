a77d0c9b6df3e5da9ec70a26dd1e9e7303f9f6a9
NeeeRL <kenken.s5049@gmail.com>
2024-09-04 21:11:00 +0900

    動的にBoxの高さを指定するようにした

--- js/others/ichinoseki.js
+++ js/others/ichinoseki.js
@@ -29,3 +29,33 @@ function changeYoubi() {
 youbi.addEventListener("change", changeYoubi);
 
 changeYoubi;
+
+// 高さを設定する
+
+function setBoxHeight() {
+    // メディアクエリで指定された幅以上のデバイスに対してのみ高さを設定
+    if (window.innerWidth >= 1000) {
+        // busStopName IDの高さを取得
+        const busStopElement = document.getElementById('busStopName');
+        const busStopHeight = busStopElement.offsetHeight;
+
+        // ビューポートの高さ (100vh) を取得
+        const viewportHeight = window.innerHeight;
+
+        // 100vh から busStopName の高さを引いた値を計算
+        const newHeight = viewportHeight - busStopHeight;
+
+        // 高さが負の値にならないようにする
+        const boxElement = document.getElementById('Box');
+        boxElement.style.height = (newHeight > 0 ? newHeight : 0) + 'px';
+    } else {
+        // 幅が600px未満のデバイスでは高さの設定を解除
+        const boxElement = document.getElementById('Box');
+        boxElement.style.height = 'auto';
+    }
+}
+
+// ページロード時とリサイズ時に高さを調整
+window.onload = setBoxHeight;
+window.onresize = setBoxHeight;
+
