9ca9d355f5de6a4c2dbc9446df94d5aa884561a6
tatsuki-s <zekoushn@gmail.com>
2024-11-05 10:35:32 +0900

    祇園・平泉駅前の画像を追加

--- server/server.js
+++ server/server.js
@@ -5,20 +5,20 @@ const port = 3000;
 const fs = require("fs");
 const path = require("path");
 
+
+const topPage = path.join(__dirname, "../web/TopPage.html");
+
+
 //読み込むディレクトリを全て指定
 app.use(express.static(path.join(__dirname, "../web")));
 
-app.get("/TopPage.html", (req, res) => {
-	res.redirect("/");
-});
 
 app.get("/", (req, res) => {
 	//__dirnameで絶対パスに変換
-	const topPage = path.join(__dirname, "../web/TopPage.html");
 	fs.readFile(topPage, (err, data) => {
 		//読み込み失敗時の動作
 		if(err) {
-			res.status(500).send("File not found");
+			res.status(404).send("File not found");
 			//ファイルの読み込み成功時の動作
 		} else {
 			//読み込んだ内容を文字列に変換してクライアントに送信
@@ -27,6 +27,15 @@ app.get("/", (req, res) => {
 			console.log(data);
 		}
 	});
+	
+});
+
+
+
+
+//動作してないけど一旦放置
+app.get(topPage, (req, res) => {
+	res.redirect("/");
 });
 
 app.listen(port, () => {
