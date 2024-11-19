c9450ee7c8b441c4058b0a741b9858f8e95b60fa
NeeeRL <kenken.s5049@gmail.com>
2024-11-19 19:48:24 +0900

    いらないjsコードを消した。

--- web/js/usual/headerAndFooter.js
+++ web/js/usual/headerAndFooter.js
@@ -5,32 +5,3 @@ fetch("/header.html")
 fetch("/footer.html")
 .then((response) => response.text())
 .then((data) => document.querySelector("#footer").innerHTML = data);
-
-//どこをクリックしてもメニューが開くように
-
-//こいつがないとチェックボックス検知できない
-document.addEventListener("DOMContentLoaded", () => {
-    //全てのクリックを検知
-    document.addEventListener("click", (event) => {
-        
-        //メニューボタンを取得
-        const menuButton = document.querySelector("label.menu-btn");
-        //メニュー全体を取得
-        const menu = document.querySelector("#menu-content");
-
-        //メニューを閉じる関数
-        function closeMenu() {
-            menuButton.checked = false;
-            //alert("closed");
-        }
-
-        //メニューを開く関数
-        function openMenu() {
-            menuButton.checked = true;
-            //alert("opened");
-        }
-
-        //ここにif分でメニューを開く条件を書く
-
-    });
-});
