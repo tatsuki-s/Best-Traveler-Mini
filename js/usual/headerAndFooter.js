2fd587aca4fa78c9d4ae14e183fe951ddfdbfda9
tatsuki-s <zekoushn@gmail.com>
2024-12-08 08:50:23 +0900

    ヘッダーの位置を修正

--- js/usual/headerAndFooter.js
+++ js/usual/headerAndFooter.js
@@ -1,7 +1,7 @@
 //ヘッダーとフッターの読み込み
-fetch("/header.html")
+fetch("best-traveler-mini/header.html")
 .then((response) => response.text())
 .then((data) => document.querySelector("#header").innerHTML = data);
-fetch("/footer.html")
+fetch("best-traveler-mini/footer.html")
 .then((response) => response.text())
 .then((data) => document.querySelector("#footer").innerHTML = data);
