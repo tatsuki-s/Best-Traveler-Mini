d82b9b45c3ccac961a3f59c482b3ff43275068a6
tatsuki-s <zekoushn@gmail.com>
2024-12-08 09:03:04 +0900

    再びヘッダーの位置のスペルミスの修正

--- js/usual/headerAndFooter.js
+++ js/usual/headerAndFooter.js
@@ -1,7 +1,7 @@
 //ヘッダーとフッターの読み込み
-fetch("Best-Traveler-Mini/header.html")
+fetch("/header.html")
 .then((response) => response.text())
 .then((data) => document.querySelector("#header").innerHTML = data);
-fetch("Best-Traveler-Mini/footer.html")
+fetch("/footer.html")
 .then((response) => response.text())
 .then((data) => document.querySelector("#footer").innerHTML = data);
