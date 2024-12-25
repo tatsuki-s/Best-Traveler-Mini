b35ece8d328236e206a18cbd559722ddfc200a50
tatsuki-s <zekoushn@gmail.com>
2024-12-25 09:35:03 +0900

    all remove

--- web/js/usual/headerAndFooter.js
+++
@@ -1,7 +0,0 @@
-//ヘッダーとフッターの読み込み
-fetch("/header.html")
-.then((response) => response.text())
-.then((data) => document.querySelector("#header").innerHTML = data);
-fetch("/footer.html")
-.then((response) => response.text())
-.then((data) => document.querySelector("#footer").innerHTML = data);
