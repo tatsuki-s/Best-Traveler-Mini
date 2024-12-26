646fa51de47836821e0f4c980a88b62f608ce262
tatsuki-s <zekoushn@gmail.com>
2024-12-26 12:36:21 +0900

    スライド周りを設定

--- src/views/stops/stopPages/picture.vue
+++ src/views/stops/stopPages/picture.vue
@@ -11,10 +11,31 @@ import { Splide, SplideSlide } from '@splidejs/vue-splide';
 
 //const showLightbox = ref(false);
 
+
+const main = ref(null);
+const thumbs = ref(null);
+
+onMounted(() => {
+  const thumbsSplide = thumbs.value?.splide;
+
+  if (thumbsSplide) {
+    main.value?.sync(thumbsSplide);
+  }
+});
+
 </script>
 <template>
     <pictures>
-		  <Splide :options="{ rewind: true, gap: '1rem', autoplay: true }">
+		  <Splide :options="{ rewind: true, gap: '1rem', isNavigation: true, perPage: 2, arrows: false, drag: false }" ref="thumbs" id="thumbs">
+			<SplideSlide>
+				<p><span class="menuBar">中尊寺・イオン前沢&nbsp;</span>方面</p>
+			</SplideSlide>
+			<SplideSlide>
+				<p><span class="menuBar">一関駅前&nbsp;</span>方面</p>
+			</SplideSlide>
+		  </Splide>
+
+		  <Splide :options="{ rewind: true, gap: '1rem', autoplay: false, type: 'loop', arrows: false}" ref="main">
 			<SplideSlide>
 				<viewer>
 			 	 <img src="/stopImg/osa-hiraizumi.jpg" alt="StopB">
@@ -31,5 +52,14 @@ import { Splide, SplideSlide } from '@splidejs/vue-splide';
 <style scoped>
 img {
 	width:100%;
+}
+
+thumbs {
+	width: 30%;
+}
+
+.menuBar {
+	font-weight: bold;
+	font-size: 120%;
 }
 </style>
